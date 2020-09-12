d1f59e4f01761d70e0692581cdc8b78e44b512fc
PappooSingh <32462523+PappooSingh@users.noreply.github.com>
2020-09-12 13:05:05 +0200

    Add files via upload
    updated files

---
+++ src/main/java/com/demo/brainwaive/config/MyCorsConfiguration.java
@@ -0,0 +1,39 @@
+package com.demo.brainwaive.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class MyCorsConfiguration implements WebMvcConfigurer{
+	/*@Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        //config.addAllowedOrigin("http://127.0.0.1:9000");
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
+        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }*/
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST");
+    }
+	
+}
--- src/main/java/com/demo/brainwaive/controller/InterestPayment.java
+++ src/main/java/com/demo/brainwaive/controller/InterestPayment.java
@@ -1,7 +1,10 @@
 package com.demo.brainwaive.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +23,9 @@ public class InterestPayment {
 	private PaymentService paymentService;
 	
 	@GetMapping("/interest")
-	public Customer getInterest(){
-		
+	public Customer getInterest(final HttpServletResponse response){
+		response.setHeader("Cache-Control", "no-cache");
+		 response.setHeader("Access-Control-Allow-Origin", "*");
 		GoldLoanPayVO goldLoanPay = new GoldLoanPayVO();
 		goldLoanPay.setAmountDue(10);
 		goldLoanPay.setLoanNumber(123);
@@ -29,9 +33,14 @@ public class InterestPayment {
 		return customer;
 	}
 	
-	@PostMapping("/interestpay")
-	public Customer payInterest(@RequestBody GoldLoanPayVO goldLoanPay){
+	@PostMapping(value="/interestpay", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Customer payInterest(@RequestBody GoldLoanPayVO goldLoanPay, final HttpServletResponse response){
 		System.out.println("payInterest!!\n");
+		 response.setHeader("Cache-Control", "no-cache");
+		 response.setHeader("Access-Control-Allow-Origin", "*");
+		 response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+		 response.setHeader("Access-Control-Allow-Credentials", "true");
+		 response.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization");
 		System.out.println("customer id : "+ goldLoanPay.getLoanNumber());
 		Customer customer = null;
 		if(null != goldLoanPay && goldLoanPay.getLoanNumber() > 0){
